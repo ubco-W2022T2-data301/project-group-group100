6d2c777c74af69726db11e7dec7c9104ab366821
Shikari King <108302860+shikariking16@users.noreply.github.com>
2023-04-05 20:21:10 -0700

    Changed some markdown text for better arranging my analysis, as well as created a function for creating a bar grapgh based on region and another column in the data.

--- scripts/project_functions2.py
+++ scripts/project_functions2.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import seaborn as sns
 import geopandas as gpd
 import geoplot as gplt
-
 
 def create_world_map_happiness(date_year_data_frame_happiness, title):
     """
@@ -40,4 +38,20 @@ def create_world_map_happiness(date_year_data_frame_happiness, title):
     ax.set_title(title, fontsize = 20)
     plt.show()
     
+    return None
+
+def region_barplot(DataAllRegion,column_name):
+    region_data = DataAllRegion.groupby('Region').apply(lambda x: pd.Series({column_name: x[column_name].mean()})).reset_index()
+
+    # Create a horizontal bar plot
+    fig, ax = plt.subplots(figsize=(12, 8))
+    sns.barplot(x=column_name, y='Region', data=region_data, ax=ax)
+
+    # Set the axis labels and title
+    ax.set_xlabel(column_name, fontsize=16)
+    ax.set_ylabel('Region', fontsize=16)
+    ax.set_title(f'Mean {column_name} Score by Region from 2015-2021', fontsize=20)
+
+    # Show the plot
+    plt.show()
     return None
